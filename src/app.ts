8bf6b455d4a6a1c42152ed0a143fdb9efb54b197
shohag roy <pkshohag240@gmail.com>
2023-07-15 02:56:59 +0600

    global error handelar added

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
+import httpStatus from "http-status";
+import globalErrorHandler from "./middlewares/globalErrorHandelar";
 
 const app = express();
 
@@ -7,6 +9,12 @@ app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Simple Book Store Server is Running...");
+});
+
+app.use(globalErrorHandler);
+
+app.all("*", (req: Request, res: Response) => {
+  res.status(httpStatus.NOT_FOUND).json({ message: "no route found" });
 });
 
 export default app;
---
+++ src/middlewares/GlobalErrorHandelar.ts
@@ -0,0 +1,60 @@
+import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import { Error } from "mongoose";
+
+import ApiError from "../errorHandelars/ApiError";
+import handleValidationError from "../errorHandelars/HandleValidationError";
+import { IGenericErrorMessage } from "../inferfaces/error";
+import envConfig from "../configs/envConfig";
+
+const globalErrorHandler: ErrorRequestHandler = (
+  error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  envConfig.node_environment === "development" &&
+    console.log(`🐱‍🏍 globalErrorHandler ~~`, error);
+
+  let statusCode = 500;
+  let message = "Something went wrong!";
+  let errorMessages: IGenericErrorMessage[] = [];
+
+  if (error?.name === "ValidationError") {
+    const simplifiedError = handleValidationError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (error instanceof ApiError) {
+    statusCode = error?.statusCode;
+    message = error.message;
+    errorMessages = error?.message
+      ? [
+          {
+            path: "",
+            message: error?.message,
+          },
+        ]
+      : [];
+  } else if (error instanceof Error) {
+    message = error?.message;
+    errorMessages = error?.message
+      ? [
+          {
+            path: "",
+            message: error?.message,
+          },
+        ]
+      : [];
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorMessages,
+    stack:
+      envConfig.node_environment === "development" ? error?.stack : undefined,
+  });
+  next();
+};
+
+export default globalErrorHandler;
