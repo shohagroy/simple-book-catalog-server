80c569b5cc29605e854c901904770e6b347817ef
shohag roy <pkshohag240@gmail.com>
2023-07-15 02:45:03 +0600

    book model and interface added

---
+++ dist/configs/envConfig.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const envConfig = {
+    node_environment: "development",
+    db_uri: "",
+    port: 5000,
+};
+exports.default = envConfig;
---
+++ src/modules/models/Book.models.ts
@@ -0,0 +1,67 @@
+import { Document, model, Model, Schema } from "mongoose";
+
+export interface IBook extends Document {
+  title: string;
+  author: string;
+  genre: string;
+  publicationDate: string;
+  image: string;
+  price: number;
+  rating: number;
+  addedBy: string;
+}
+
+const bookSchema: Schema<IBook> = new Schema<IBook>(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    author: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    publicationDate: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    addedBy: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Book: Model<IBook> = model<IBook>("Book", bookSchema);
+
+export default Book;
